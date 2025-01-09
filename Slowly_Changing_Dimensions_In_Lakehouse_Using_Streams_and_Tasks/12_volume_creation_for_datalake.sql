101830c3f0e416f19ecd098e76615878c44b50c1
yunqiqiliang <132561395+yunqiqiliang@users.noreply.github.com>
2025-01-09 20:55:13 +0800

    Update 12_volume_creation_for_datalake.sql

--- Slowly_Changing_Dimensions_In_Lakehouse_Using_Streams_and_Tasks/12_volume_creation_for_datalake.sql
+++ Slowly_Changing_Dimensions_In_Lakehouse_Using_Streams_and_Tasks/12_volume_creation_for_datalake.sql
@@ -6,6 +6,16 @@
 USE VCLUSTER SCD_VC;
 USE SCHEMA SCD_SCH;
 
+--external data lake
+--创建数据湖Connection,到数据湖的连接
+CREATE STORAGE CONNECTION if not exists hz_ingestion_demo
+    TYPE oss
+    ENDPOINT = 'oss-cn-hangzhou-internal.aliyuncs.com'
+    access_id = '请输入您的access_id'
+    access_key = '请输入您的access_key'
+    comments = 'hangzhou oss private endpoint for ingest demo'
+
+
 --创建Volume,数据湖存储文件的位置
 CREATE EXTERNAL VOLUME  if not exists scd_demo
   LOCATION 'oss://czsampledatahz/scd_demo' 
